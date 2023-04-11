1ba267834d1241c7a2b5db81af5359ebb7b726ca
tatatacicici <tamam.alfauzan@gmail.com>
2023-04-11 16:13:01 +0700

    ok

--- src/main/java/tokokue/tokokue/Basah.java
+++ src/main/java/tokokue/tokokue/Basah.java
@@ -10,11 +10,19 @@ package tokokue.tokokue;
  * @author Asus
  */
 public class Basah {
+<<<<<<< Updated upstream
     String nama, harga, id;
     
     public Basah(String nama, String harga, String id){
         this.nama=nama;
         this.harga=harga;
         this.id=id;
+=======
+    String nama_basah, harga_basah, kode_basah;
+    
+    public Basah(String nama_basah, int harga_basah, String kode_basah){
+        this.nama_basah=nama_basah;
+        this.kode_basah=kode_basah;
+>>>>>>> Stashed changes
     }
 }
