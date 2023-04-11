5d682fd3ae650d5333e873bac53a73bb6788f695
tatatacicici <tamam.alfauzan@gmail.com>
2023-04-11 20:09:01 +0700

    Perbaikan class elemen_cake

--- src/main/java/tokokue/tokokue/Elemen_cake.java
+++ src/main/java/tokokue/tokokue/Elemen_cake.java
@@ -13,7 +13,6 @@ public class Elemen_cake {
     Cake info;
     Elemen_cake next;
     Elemen_cake prev;
-
     Elemen_cake(String nama_cake, int harga_cake, String kode_cake){
         info = new Cake(nama_cake,harga_cake,kode_cake);
         next=null;
