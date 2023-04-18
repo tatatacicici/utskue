228965bad5e5fdb640a76910249e1c07bbceceda
tatatacicici <tamam.alfauzan@gmail.com>
2023-04-18 22:11:59 +0700

    Buat Class Jual

---
+++ src/main/java/tokokue/tokokue/Jual.java
@@ -0,0 +1,16 @@
+package tokokue.tokokue;
+
+/**
+ *
+ * @author Hussain & Bayu
+ */
+public class Jual {
+    String nama_produk, kode_produk;
+    int harga_produk;
+    
+    public Jual(String nama_produk, int harga_produk, String kode_produk){
+        this.nama_produk = nama_produk;
+        this.kode_produk = kode_produk;
+    }
+    
+}
