208061b2bbb22d51238a4ba312ad81b4ae17969f
tatatacicici <tamam.alfauzan@gmail.com>
2023-04-19 11:27:43 +0700

    List_jual [NEW]

--- src/main/java/tokokue/tokokue/List_jual.java
+++ src/main/java/tokokue/tokokue/List_jual.java
@@ -5,17 +5,25 @@ package tokokue.tokokue;
  * @author Hussain & Bayu
  */
 public class List_jual {
-   Elemen_cake cake;
-    Elemen_kering kering;
-    Elemen_basah basah;
-    Elemen_jual first;
+
+Elemen_jual first;
+List_jual(){
+    first = null;
+}
     
-    List_jual(){
-        cake = new Elemen_cake(nama_cake, harga_cake, kode_cake);
-        kering = new Elemen_kering(nama_kering, harga_kering, kode_kering);
-        basah = new Elemen_basah(nama_basah, harga_basah, kode_basah);
-        first = null;
+    void tambahCake(Cake cake){
+       Elemen_jual elemenbaru= new Elemen_jual(cake);
+       Elemen_jual elemensementara = first;
+       if (first == null){
+           first = elemenbaru;
+       }
+        while(elemensementara.next != null){
+            elemensementara = elemensementara.next;
+        }
+        elemensementara.next = elemenbaru;
+        elemenbaru.prev = elemensementara;
+       }
     }
     
     
-}
+
