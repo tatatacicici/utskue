b485996a94e146bccf20a749ce4726111d5f03ff
tatatacicici <tamam.alfauzan@gmail.com>
2023-04-19 11:53:33 +0700

    List_jual [Penambahan method menambahkan kue kering dan kue basah]

--- src/main/java/tokokue/tokokue/List_jual.java
+++ src/main/java/tokokue/tokokue/List_jual.java
@@ -23,6 +23,31 @@ List_jual(){
         elemensementara.next = elemenbaru;
         elemenbaru.prev = elemensementara;
        }
+    void tambahKering(Kering kkering){
+       Elemen_jual elemenbaru= new Elemen_jual(kkering);
+       Elemen_jual elemensementara = first;
+       if (first == null){
+           first = elemenbaru;
+       }
+        while(elemensementara.next != null){
+            elemensementara = elemensementara.next;
+        }
+        elemensementara.next = elemenbaru;
+        elemenbaru.prev = elemensementara;
+       }
+    void tambahBasah(Basah kbasah){
+        Elemen_jual elemenbaru = new Elemen_jual(kbasah);
+        Elemen_jual elemensementara = first;
+       if (first == null){
+           first = elemenbaru;
+       }
+        while(elemensementara.next != null){
+            elemensementara = elemensementara.next;
+        }
+        elemensementara.next = elemenbaru;
+        elemenbaru.prev = elemensementara;
+       }
+    
     }
     
     
