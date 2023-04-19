78abe0edec22e06bcd9286913fce0f22c55817c7
tatatacicici <tamam.alfauzan@gmail.com>
2023-04-19 11:26:53 +0700

    Jual v.2

--- src/main/java/tokokue/tokokue/Jual.java
+++ src/main/java/tokokue/tokokue/Jual.java
@@ -5,12 +5,10 @@ package tokokue.tokokue;
  * @author Hussain & Bayu
  */
 public class Jual {
-    String nama_produk, kode_produk;
-    int harga_produk;
+    Object info;
     
-    public Jual(String nama_produk, int harga_produk, String kode_produk){
-        this.nama_produk = nama_produk;
-        this.kode_produk = kode_produk;
+    public Jual(Object info){
+        this.info = info;
     }
     
 }
