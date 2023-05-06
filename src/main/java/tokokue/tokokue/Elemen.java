08f27f78ec3a076207e3f916de8642c640cc97d9
tatatacicici <tamam.alfauzan@gmail.com>
2023-05-06 23:57:35 +0700

    Elemen v.0

--- src/main/java/tokokue/tokokue/Elemen.java
+++ src/main/java/tokokue/tokokue/Elemen.java
@@ -45,6 +45,39 @@ public class Elemen {
         }
                 
     }
+    public static class Move_cake{
+        Info.Cake info_cake;
+        Move_cake next;
+        Move_cake prev;
+
+        public Move_cake(String nama_cake, int harga_cake, String kode_cake){
+            info_cake = new Info.Cake(nama_cake, harga_cake, kode_cake);
+            next = null;
+            prev = null;
+        }
+    }
+    public static class Move_kering{
+        Info.Kering info_kering;
+        Move_kering next;
+        Move_kering prev;
+
+        public Move_kering(String nama_kering, int harga_kering, String kode_kering){
+            info_kering = new Info.Kering(nama_kering, harga_kering, kode_kering);
+            next = null;
+            prev = null;
+        }
       
     }
+    public static class Move_basah{
+        Info.Basah info_basah;
+        Move_basah next;
+        Move_basah prev;
 
+        public Move_basah(String nama_basah, int harga_basah, String kode_basah){
+            info_basah = new Info.Basah(nama_basah, harga_basah, kode_basah);
+            next = null;
+            prev = null;
+        }
+
+    }
+}
